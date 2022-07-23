ba8f03dc3bc13710e53e5238fa79411cf8f3322a
YashFafolawala <54712476+YashFafolawala@users.noreply.github.com>
2022-07-23 12:20:01 +0200

    Modify logic of getting correct datapoints

--- app/meshtasticdataprocessing.py
+++ app/meshtasticdataprocessing.py
@@ -27,7 +27,8 @@ cr = []
 symLen = []
 totalpayloadSize = []
 airtime = []
-
+id = []
+mode = []
 # r"" is used to consider path as a raw string. 
 # 2nd option use double backslash. Else it gives unicode error
 # Define App class 
@@ -49,6 +50,10 @@ class App(tkinter.Tk):
                             activeforeground='white', activebackground='#46403E', 
                             command=lambda:self.processSerialLogs(fpath))
         PrcSerialLogsButton.place(x=25, y=100)
+        # quit gui
+        QuitButton = Button(self, text="Quit", activeforeground='white', activebackground='#46403E', 
+                    command=self.destroy)
+        QuitButton.place(x=230, y=250)
 
     def selectFile(self):
         global fpath
@@ -66,46 +71,50 @@ class App(tkinter.Tk):
         append_count = 0
         dc = 0
         for f in range(len(rawLogs)):
-            log_lines.append(re.sub(",", "", rawLogs[f]))
+            log_lines.append(rawLogs[f].replace(",", "", 2))
             pos_msg = rawLogs[f].find("msg=")
             if pos_msg > 65:
                 # get the data points from a line
-                sub_wordlist = log_lines[f].replace(",", "",2).split()
+                sub_wordlist = log_lines[f].split()
+                #print(sub_wordlist)
+                id.append(int(sub_wordlist[1]))
                 sender.append(sub_wordlist[6].replace("from=", ""))
                 requestid.append(int(sub_wordlist[7].replace("id=", ""), 0))
                 #nlist = " ".join([sub_wordlist[x].replace("msg=", "") for x in range(7, len(sub_wordlist), 1)])
-                textmsg.append(" ".join([sub_wordlist[x].replace("msg=", "") for x in range(8, len(sub_wordlist), 1)]))
+                textmsg.append(" ".join([sub_wordlist[x].replace("msg=", "") \
+                                for x in range(8, len(sub_wordlist), 1)]))
                 
-                if f-3 > 0:
-                    sub_wordlist2 = log_lines[f-3].replace("(", "").replace(")", "").split()
-                    receiver.append(sub_wordlist2[7])
-                    hoplimit.append(int(sub_wordlist2[9].replace("HopLim", "")))
-                    channelid.append(int(sub_wordlist2[10].replace("Ch", ""), 0))
-                    rxtimestampunix.append(int(sub_wordlist2[12].replace("rxtime=", "")))
-                    # "\" operator indicates that the statement is continued in next line
-                    print(rxtimestampunix)
-                    print(append_count)
-                    rxdatetime.append(datetime.datetime.fromtimestamp\
-                                (rxtimestampunix[append_count], datetime.timezone.utc))
-                    rxSNR.append(float(sub_wordlist2[13].replace("rxSNR=", "")))
-                else:
-                    print("not enough data lines to get required data points (-3)")
-                if f-10 > 0:
-                    sub_wordlist3 = log_lines[f-10].replace("(", "").replace(")", "").\
-                                    replace(",", "").split()  
-                    bw.append(int(sub_wordlist3[3].replace("bw=", "")))
-                    sf.append(int(sub_wordlist3[4].replace("sf=", "")))
-                    cr.append(sub_wordlist3[5].replace("cr=", "")) 
-                    symLen.append(int(sub_wordlist3[7].replace("symLen=", "")))
-                    totalpayloadSize.append(int(sub_wordlist3[9].replace("payloadSize=", "")))
-                    airtime.append(int(sub_wordlist3[11]))
-                else:
-                    print("not enough data lines to get required data points (-10)")
-                
+                # get other data points via tracking the same id
+                for f2 in range(4):
+                    if log_lines[f-f2].find("decoded message") > 0 and \
+                    log_lines[f-f2].find(str(id[append_count])):  
+                        sub_wordlist2 = log_lines[f-f2].replace("(", "").replace(")", "").split()
+                        receiver.append(sub_wordlist2[7])
+                        hoplimit.append(int(sub_wordlist2[9].replace("HopLim", "")))
+                        channelid.append(int(sub_wordlist2[10].replace("Ch", ""), 0))
+                        rxtimestampunix.append(int(sub_wordlist2[12].replace("rxtime=", "")))
+                        # "\" operator indicates that the statement is continued in next line
+                        rxdatetime.append(datetime.datetime.fromtimestamp\
+                                    (rxtimestampunix[append_count], datetime.timezone.utc))
+                        rxSNR.append(float(sub_wordlist2[13].replace("rxSNR=", "")))
+                    
+                for f3 in range(15):
+                    if log_lines[f-f3].find("bw=") > 0 and \
+                    log_lines[f-f3].find(str(id[append_count])): 
+                        sub_wordlist3 = log_lines[f-f3].replace("(", "").replace(")", "").\
+                                        replace(",", "").split()  
+                        bw.append(int(sub_wordlist3[3].replace("bw=", "")))
+                        sf.append(int(sub_wordlist3[4].replace("sf=", "")))
+                        cr.append(int(sub_wordlist3[5].replace("cr=", ""))) 
+                        symLen.append(sub_wordlist3[7].replace("symLen=", ""))
+                        totalpayloadSize.append(int(sub_wordlist3[9].replace("payloadSize=", "")))
+                        airtime.append(int(sub_wordlist3[11]))
+                    
                 append_count += 1
             # elif pos_msg < 65 and pos_msg > 50:
             #     dc += 1       
         print(append_count)
+        print(textmsg)
         header = ['Request ID','Rx Time UTC' , 'Rx Timestamp', 'Sender', 'msg',\
              'Total Payload Size (bytes)', 'Air Time (ms)', 'Hop Limit', 'Channel ID',\
                  'Bandwidth (khZ)', 'Spreading Factor', 'Coding Rate', 'symLen (ms)']
